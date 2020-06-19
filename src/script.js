15a88ffe932639e47d22c5153809d9882eac8b23
Phil Billingsby <philbillingsby@gmail.com>
2020-06-19 14:15:10 -0400

    Get values from forms

--- src/script.js
+++ src/script.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 })
-const newForm = `
-<h4>Worker</h4>
-<div class="form-group">
-  <input type="text" name="worker" class="" placeholder="Worker Name">
-</div>
-<div class="form-group">
-  <input type="number" name="hours" placeholder="Hours Worked">
-</div>`;
 document.addEventListener('DOMContentLoaded', () => {
   
 })
 
 function addWorker(value) {
   document.getElementById('form-block').innerHTML = "";
-
   for (i = 0; i < value; i++) {
+    const newForm = `
+  <h4>Worker ${i + 1}</h4>
+  <div class="form-group">
+    <input type="text" name="worker" class="" placeholder="Worker Name">
+  </div>
+  <div class="form-group">
+    <input type="number" name="hours" placeholder="Hours Worked">
+  </div>`;
     document.getElementById('form-block').innerHTML += newForm;
   }
 }
@@ -23,7 +22,7 @@ function addWorker(value) {
 
 function calculateTips() {
   cashTip = parseInt(document.getElementById('cashTips').value)
-  debugger
+  document.querySelectorAll('input').forEach(obj => console.log(obj.value))
 }
 
 // FIX TO ADD WORKER WITHOUT
