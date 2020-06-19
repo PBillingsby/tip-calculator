ce3ef70d82abf40537f3221a1020534b4b8f5241
Phil Billingsby <philbillingsby@gmail.com>
2020-06-19 16:00:53 -0400

    Fix output on submit

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  addWorker(1);
-})
-
 function addWorker(value) {
-  document.getElementById('form-block').innerHTML = "";
+  const form = document.getElementById('form-block');
+  form.addEventListener("submit", () => {
+    event.preventDefault();
+  })
+  document.getElementById('form-block').innerHTML = `<input type="number" placeholder="Cash Amount" id="cashTips"><br>
+  <input  type="submit" placeholder="Add Cash" onclick="calculateTips()">`;
   for (i = 0; i < value; i++) {
+    
     const newForm = `
-  <div id="worker[${i + 1}]">
+  <div id="worker[${i + 1}]" class="worker-div">
     <h4>Worker ${i + 1}</h4>
     <div class="form-group">
       <input type="text" name="worker" class="" placeholder="Worker Name">
@@ -19,15 +21,25 @@ function addWorker(value) {
   }
 }
 
-
 function calculateTips() {
+  let workers = [];
   let cashTip = parseInt(document.getElementById('cashTips').value)
-  let openHours = parseInt(document.getElementById('openHours').value)
-  debugger
-  let formBlockCount = document.getElementById(`form-block`);
-  for (let i = 0; i < formBlockCount.childElementCount; ++i) {
-    console.log(document.getElementById(`worker[${i}]`))
+  for (let i = 0; i < document.getElementsByClassName('worker-div').length; i++) {
+    let workerInfo = {
+      name: document.getElementById(`worker[${i + 1}]`).querySelector('input').value,
+      hours: parseInt(document.getElementById(`worker[${i + 1}]`).querySelectorAll('input')[1].value) // Multiply by hourly 
+    }
+    workers.push(workerInfo) 
   }
+  returnTips(workers, cashTip);
 }
 
-// FIX TO ADD WORKER WITHOUT
+function returnTips(arr, cashTips) {
+  let count = 0;
+  arr.forEach(obj => count += obj.hours)
+  const hourly = cashTips / count;
+  arr.forEach(worker => {
+    document.getElementById('results').innerHTML += `<h4>${worker.name} you made $${parseFloat(worker.hours * hourly).toFixed(2)}</h4>`;
+  })
+  document.getElementById('form-block').innerHTML += ` <button type="submit" onclick="location.reload()">Reload</button>`
+}
